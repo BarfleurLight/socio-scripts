bc9bb39b8456770891d1e6b4a5a6f3d7c1761cb2
BarfleurLight <obrishtigoogl@gmail.com>
2025-06-12 13:33:05 +0300

    feat(driver): add check root

--- driver-sharp-mx266n/install.sh
+++ driver-sharp-mx266n/install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 
-main() { 
+check_if_running_as_root() {
+  if [[ "$(id -u)" -eq 0 ]]; then
+    return 0
+  else
+    echo "error: You must run this script as root!"
+    return 1
+  fi
+}
 
+main() { 
+  check_if_running_as_root || return 1
 }
 
 main "$@"
